453f75031d69cb9e9231295470d2ae38c4518ded
pzych <pawel.zych@gmail.com>
2018-12-10 21:58:42 +0100

    added configuration for used pins . so it is easier to make a ciruit based on this config

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import { startHTTP } from "./http-server";
-import { getDistance } from "./distance-sensor";
-import { alarm } from "./alarm";
-import {watchIsWater} from "./water-sensor";
+import {GPIO_CONFIG} from "./gpio.config";
+
+require('dotenv').config();
+// import { startHTTP } from "./notifications/http-server";
+// import { alarm } from "./notifications/alarm";
+// import { getDistance } from "./sensors/distance-sensor";
+import {watchIsWater} from "./sensors/water-sensor";
+// import {firebaseApp} from '../firebase/init.js';
+import {watchIsMotion } from "./sensors/motion-sensor";
+
+//  watchIsMotion().subscribe((val)=> {console.log(` motion!! ${val}`)})
+watchIsMotion(GPIO_CONFIG.MOTION_SENSOR).subscribe((val)=> {console.log(` motion ${val}`)})
+// startHTTP().then(function(socket){
+//     firebase.database().ref('/posts').on('child_added', function(postSnapshot) {
+//         console.log(postSnapshot);
+//       });
+
+// });
 
 
-watchIsWater().subscribe((val)=> {console.log(val)})
-startHTTP().then(function(socket){
-    // setInterval(updateDistance(socket),500);
-
-});
-
-
-// watchIsWater().subscribe((val)=> {console.log(val)})
-function updateDistance(socket){
-    return function(){
-        getDistance().then(function(distanceOutput){
-            console.log(distanceOutput)
-            socket.broadcast.emit('distance', distanceOutput);
-            if(distanceOutput > 50){
-                alarm();
-            }
-        });
-    }
+// // watchIsWater().subscribe((val)=> {console.log(val)})
+// function updateDistance(socket){
+//     return function(){
+//         getDistance().then(function(distanceOutput){
+//             console.log(distanceOutput)
+//             socket.broadcast.emit('distance', distanceOutput);
+//             if(distanceOutput > 50){
+//                 alarm();
+//             }
+//         });
+//     }
     
-}
+// }
--- src/sensors/motion-sensor.js
+++ src/sensors/motion-sensor.js
@@ -1,11 +1,11 @@
 import { Observable} from 'rxjs';
 
-export function watchIsWater(gpioNo) {
+export function watchIsMotion(gpioNo) {
     const gpio = require('rpi-gpio');
-    gpio.setup(gpioNo, gpio.DIR_IN, gpio.EDGE_BOTH, () => console.log('WATER SENSOR INITIALIZED'));
+    gpio.setup(gpioNo, gpio.DIR_IN, gpio.EDGE_BOTH, () => console.log('MOTION SENSOR INITIALIZED'));
     return Observable.create(function(observer) {
         gpio.on('change', function(channel, value) {
             observer.next(value);
         });
-    });
+    });  
  }
