cf0b0ca33591abf8d943c815f75a3f12f878ddfa
pzych <pawel.zych@gmail.com>
2018-12-20 20:34:44 +0100

    replaced executing external command with other nodejs ported library because it is much more faster

--- src/gpio/gpio-read.js
+++ src/gpio/gpio-read.js
@@ -1,21 +1,24 @@
-import {Observable} from 'rxjs';
+import {first, Observable, tap} from 'rxjs';
 import {map, filter} from "rxjs/operators";
 
 const gpio = require('rpi-gpio');
 
+const bindObserverToChange = (observer) => {
+    return (channel, value) => {
+        observer.next({channel, value})
+    }
+
+}
 const GPIOState$ = Observable.create(
     (observer) => {
-        gpio.on('change', ({channel, value}) => {
-            observer.next({channel, value})
-        })
+        gpio.on('change', bindObserverToChange(observer));
     }
 )
 
-
-export function gpioWatch(gpioPhysicalNo, intervalTime = 10) {
+export function gpioWatch(gpioPhysicalNo) {
     gpio.setup(gpioPhysicalNo, gpio.DIR_IN, gpio.EDGE_BOTH);
     return GPIOState$.pipe(
-        filter(({channel}) => channel != gpioPhysicalNo),
+        filter(({channel}) => channel == gpioPhysicalNo),
         map(({value}) => value)
     )
 }
