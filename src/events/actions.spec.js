75356e0694e3d8d5b12b21d3659e9b088839e207
pzych <pawel.zych@gmail.com>
2019-01-08 17:01:08 +0100

    added ntssting rxjs example using sinon

--- src/events/actions.spec.js
+++ src/events/actions.spec.js
@@ -1,7 +1,36 @@
-import { marbles } from "rxjs-marbles/mocha";
+import {fakeSchedulers, marbles} from "rxjs-marbles/mocha";
 import { map } from "rxjs/operators";
+import {timer} from "rxjs";
+import * as sinon from "sinon";
 
 describe("basic", () => {
+    it('should multiply by "2" each value emitted', marbles((m) => {
+        const values = { a: 1, b: 2, c: 3, x: 2, y: 4, z: 6};
+        const source = m.cold('-a-b-c-|', values);
+        const expected = m.cold('-x-y-z-|', values);
+        const result = source.pipe(map(x => x*2));
+        m.expect(result).toBeObservable(expected);
+    }));
+
+
+    let clock;
+
+    beforeEach(() => {
+        clock = sinon.useFakeTimers();
+    });
+
+    it("should be testable with fake time", fakeSchedulers(() => {
+        let received;
+        timer(100).subscribe(value => received = value);
+        clock.tick(50);
+
+        clock.tick(50);
+        expect(received).to.equal(0);
+    }));
+
+    afterEach(() => {
+        clock.restore();
+    });
 
     it("should support marble tests without values", marbles(m => {
 
