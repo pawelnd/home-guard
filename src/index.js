fb152988bc3fb0f5db40d49d7696e8754224a39b
pzych <pawel.zych@gmail.com>
2019-01-10 23:21:17 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -1,44 +1,21 @@
-import {getBuzzerPlayer} from "./notifications/buzzer/buzzer-player";
-
 require('dotenv').config();
 
-import {GPIO_CONFIG} from "./gpio.config";
-import {firebaseApp} from "./firebase-init";
-import {startHTTP} from "./notifications/http-server";
-import {watchTempHumidity$} from "./sensors/temp-humidity/temp-humidity";
 import {combineLatest} from "rxjs";
-import {watchButton, watchDoorOpen, watchIsMotion$, watchIsWater$} from "./sensors/sensors";
-import {distinctUntilChanged} from "rxjs/operators";
+import {watchButton, watchDoorsAreOpen, watchIsMotion$} from "./sensors/sensors";
+import {getBuzzerPlayer} from "./notifications/buzzer/buzzer-player";
+import {GPIO_CONFIG} from "./gpio.config";
 import {BUZZER_MELODIES} from "./notifications/buzzer/buzzer-melodies";
-import {getPinOutputController} from "./gpio/gpio-write";
 
-
-
-const doorOpen$ = watchDoorOpen(GPIO_CONFIG.ACTION_BUTTON);
-const buttonPressed$ = watchButton(GPIO_CONFIG.DOOR);
-const motion$ = watchIsMotion$(GPIO_CONFIG.MOTION_SENSOR);
-const water$ = watchIsWater$(GPIO_CONFIG.WATER_SENSOR);
-const watchTempHum$ = watchTempHumidity$(GPIO_CONFIG.THERMOMETER);
+let motion$ = watchIsMotion$();
+let doorOpen$ = watchDoorsAreOpen();
+let buttonPressed$ = watchButton();
 
 var i = 0;
-// const combined = combineLatest(motion$, water$, watchTempHum$, buttonPressed$,doorOpen$);
-// combined.subscribe(([isMotion,isWater,tempHum, doorsAreOpen, buttonPushed])=>{
-//     let logLine = `${i++%100} door -> ${doorsAreOpen} button -> ${buttonPushed} motion -> ${isMotion}   water -> ${isWater} temp -> ${tempHum?tempHum.temp:'none'} hum -> ${tempHum?tempHum.humidity:'none'}`
-//     console.log(logLine)
-// })
+let buzzerPlayer = getBuzzerPlayer(GPIO_CONFIG.BUZZER);
 
-let relayController = getPinOutputController(GPIO_CONFIG.RELAY);
-
-motion$.pipe(distinctUntilChanged()).subscribe((open)=>{
-    console.log(open)
-    if(!open){
-        relayController.setLow();
-    }else {
-        relayController.setHigh();
-    }
-
+const combined = combineLatest(motion$, buttonPressed$,doorOpen$);
+combined.subscribe(([isMotion,buttonPushed, doorsAreOpen])=>{
+    let logLine = `${i++%100} door -> ${doorsAreOpen} button -> ${buttonPushed} motion -> ${isMotion}`;
+    console.log(logLine);
 })
 
-// startHTTP().then(function (socket) {
-//     console.log('user connected')
-// });
